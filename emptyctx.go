17d329b283d44c58bf380237a4edfec06965386e
nickytaylor <72091686+nickytaylor@users.noreply.github.com>
2023-03-02 15:11:18 +0800

    Create emptyctx.go

---
+++ emptyctx.go
@@ -0,0 +1,32 @@
+type emptyCtx int
+
+var (
+	background = new(emptyCtx)
+	todo       = new(emptyCtx)
+)
+
+func Background() context.Context {
+	return background
+}
+
+func (*emptyCtx) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+func (*emptyCtx) Done() <-chan struct{} {
+	return nil
+}
+func (*emptyCtx) Err() error {
+	return nil
+}
+func (*emptyCtx) Value(key interface{}) interface{} {
+	return nil
+}
+func (e *emptyCtx) String() string {
+	switch e {
+	case background:
+		return "context.Background"
+	case todo:
+		return "context.TODO"
+	}
+	return "unknown empty Context"
+}
